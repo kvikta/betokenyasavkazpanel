51ac86f864d6404481ec9d6fff7ee233cc35f362
VictonicTechnologies <victorkipngetichlangat103@gmail.com>
2025-07-18 12:25:58 +0300

    Update Survey.js

--- src/screens/app/Survey.js
+++ src/screens/app/Survey.js
@@ -49,7 +49,7 @@ export default function Survey() {
     });
 
     useEffect(() => {
-        fetch('https://surveyiz.github.io/SurveysDetails/surveyiz.json')
+        fetch('https://victonictechnologies.github.io/SurveysDetails/surveys.json')
             .then(response => response.json())
             .then(data => {
                 var dataFilteredSurvey = data.surveys.filter(item => item.surveyId == location.state.Id)
